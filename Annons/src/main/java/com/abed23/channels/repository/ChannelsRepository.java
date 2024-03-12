ca241d3d4dc563c57b67380389ddc8946e5e1f76
MahmoudAbed2 <148431502+MahmoudAbed2@users.noreply.github.com>
2024-03-12 11:32:32 +0100

    Delete Annons directory

--- Annons/src/main/java/com/abed23/channels/repository/ChannelsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abed23.channels.repository;
-
-import com.abed23.channels.model.entity.Channels;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChannelsRepository extends JpaRepository<Channels, Integer> {
-}
--- Annons/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into channels (titel) values ('ahmed abed');
-insert into channels (titel) values ('omar abed');
-insert into channels (titel) values ('momo abed');
